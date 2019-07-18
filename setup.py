d8931d203e347e3de6c38e0bf94bf62c225df1e2
Cyrille GACHOT <cyrille.gachot@smile.fr>
2019-07-18 12:16:26 +0200

    Prepare version 1.1.2

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ IMPORT = 'sphinxcontrib.versioning'
 INSTALL_REQUIRES = ['click', 'colorclass', 'sphinx']
 LICENSE = 'MIT'
 NAME = 'sphinx-versions'
-VERSION = '1.1.1'
+VERSION = '1.1.2'
 
 
 def readme(path='README.rst'):
